79a0b0268f4b2e58c3a9256a16679335fbde53a4
Sherry Masutani <sherrymasutani@mail.weber.edu>
2017-02-14 16:57:53 +0000

    initial commit

---
+++ deuteron_hw5_task1.c
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  deuteron_hw5_task1.c
+ *       	Usage:  ./deuteron_hw5_task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 04:41:59 PM
+ *       Compiler:  gcc -Wall -Werr
+ *         Author:  Sherry Masutani (), sherrymasutani@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main()
+{
+
+	return 0;
+}
+
