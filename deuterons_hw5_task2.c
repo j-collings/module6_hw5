ecf0f656d1dddaf2d0d325d6a6976937924bdb23
Julie Collings <juliecollings@mail.weber.edu>
2017-02-14 17:01:05 +0000

    initial commit

---
+++ deuterons_hw5_task2.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  deuterons_hw5_task2.c
+ *       	Usage:  ./deuterons_hw5_task2.c
+ *    Description:  :w
+ *
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 04:38:07 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Julie Collings (), juliecollings@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(void)
+{
+
+	return 0;
+}
+
+
+/* Function Definitions */
+
+
